e0f9a15b29d829ef44cd398ce37156fed533884a
ktapia576 <ktapia576@hotmail.com>
2024-07-20 17:13:32 -0400

    Add methods and arrays

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,8 +1,10 @@
 public class BinarySearch {
     public static void main(String[] args) {
-        int arr = new int[10];
+        int[] arr = {1,2,3,4,5,6,7,8};
+        int[] arr2 = initializeArrayWithValues(100);
 
         printArr(arr);
+        printArr(arr2);
     }
 
     /**
@@ -15,5 +17,22 @@ public class BinarySearch {
             System.out.print(arr[i] + " ");
         }
         System.out.println(); //add new line
+    }
+
+    /**
+     * initializes array with sorted values from 0 to given length. ex. 0,1,2,3,4,5,6,..., length
+     *
+     * @param length - the max length
+     *
+     * @return int[] array with values
+     */
+    public static int[] initializeArrayWithValues(int length){
+        int[] arr = new int[length];
+
+        for(int i = 0; i < length; i++){
+            arr[i] = i;
+        }
+
+        return arr;
     }
 }
